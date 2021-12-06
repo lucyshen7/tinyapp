21b2541ae184357708a0316ca255b8e754c74195
Lucy Shen <lucyshen77@gmail.com>
2021-12-06 22:27:09 +0000

    filled out the urls_index.ejs template and rendered the URLs

--- express_server.js
+++ express_server.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
