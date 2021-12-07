7ff34357728e279f759df3dcdf7fd0f88924467f
Lucy Shen <lucyshen77@gmail.com>
2021-12-07 23:47:09 +0000

    Added form and new route to update URLs

--- express_server.js
+++ express_server.js
@@ -1,19 +1,28 @@
-const express = require("express");
-const app = express();
 const PORT = 8080; // default port 8080
-
+const express = require("express");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
+const app = express();
 app.set("view engine", "ejs");
 
+// DATA
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: true}));
+
+// ROUTES
+// Homepage route "/", callback function will be executed when this route is requested
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// Added a route for "/hello"
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // use Express render method to respond to requests by sending back a template, along with obj containing data the template needs
@@ -22,12 +31,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
-// delete URL
+// DELETE URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+// ADD New URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -35,7 +45,7 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
+  // console.log("urlDatabase", urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -44,21 +54,29 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// UPDATE URL 
+// route to handle POST request to update a resource
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  // console.log("req.body", req.body)
+  const newURL = req.body.newURL;
+  urlDatabase[shortURL] = newURL;
+  // console.log("urlDatabase", urlDatabase);
+  res.redirect("/urls");
+})
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// PORT AND CALLBACK
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
 
 // generate a random shortURL
