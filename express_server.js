e692e249484db3445925749a1555cb2a7efaabe9
Lucy Shen <lucyshen77@gmail.com>
2021-12-07 00:34:08 +0000

    Added POST route to receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,9 +16,19 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// use Express render method to respond to requests by sending back a template, along with obj containing data the template needs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Log the POST request body to the console
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -34,3 +47,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// generate a random shortURL
+function generateRandomString() {
+  const result = '';
+  const char = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 7; i++) {
+    result += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return result;
+}
